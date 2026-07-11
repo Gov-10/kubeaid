16030c343dc2444a581ad2cb78067525823a56c9
Govind <govindsys1008@gmail.com>
2026-07-11 18:25:24 +0530

    basic flow and wiring of workflow done

--- temporalWorkflow/kafka_cons.py
+++ temporalWorkflow/kafka_cons.py
@@ -3,16 +3,25 @@ from kafka import KafkaConsumer
 from temporalio.client import Client
 from dotenv import load_dotenv
 from workflows import kubeWorkflow
+from database import Execution, sessionLocal
 load_dotenv()
 
+class DatabaseError(Exception):
+    pass
+
 async def main():
+    db = sessionLocal()
     client = await Client.connect(os.getenv("TEMPORAL_URL"))
     consumer = KafkaConsumer("execution-send", bootstrap_servers= os.getenv("BOOTSTRAP_SERVER"), group_id="execution-group", value_deserializer= lambda i: json.loads(i.decode()))
     await consumer.start()
     try:
         async for msg in consumer:
-            data = msg.value 
-            exec_data = {} #TODO: DB se fetch karo
+            data = msg.value
+            template_id, task_id = data["template_id"], data["task_id"]
+            exec = db.query(Execution).filter(Execution.template_id==template_id, Execution.task_id==task_id).first()
+            if not exec:
+                raise DatabaseError("execution not found")
+            exec_data = exec.execution
             await client.start_workflow(KafkaConsumer.run(), {"template_id": data["template_id"], "task_id": data["task_id"], "task": exec_data}, id=f"kube-task-{data['task_id']}",task_queue="kube-tasks")
     finally:
         await consumer.stop()
--- temporalWorkflow/workflows.py
+++ temporalWorkflow/workflows.py
@@ -3,12 +3,23 @@ from temporalio import workflow
 from temporalio.common import RetryPolicy
 with workflow.unsafe.imports_passed_through():
     from activities import k8s_worker, http_worker, smoke_worker
+import os, json, httpx
+from dotenv import load_dotenv
+load_dotenv()
 
 @workflow.defn(name="kubeWorflow")
 class kubeWorflow:
     @workflow.run
     async def run(self, data:dict)-> str:
         custom_retry =RetryPolicy(initial_interval=timedelta(seconds=1), backoff_coefficient=2.0, maximum_interval=timedelta(seconds=30), maximum_attempts= 5, non_retryable_error_types=["ValueError"])
-        pass 
-        #TODO: TO be done later
+        for step in data.playbook["steps"]:
+            if step["type"] == "scale":
+                pass #schema fix, call k8s activity
+            if step["type"] == "health_check":
+                pass #schema fix, call http_worker
+            if step["type"] == "smoke_test":
+                pass #schemafix, call smoke_worker
+            #TODO: combine the total result
+        #TODO: return combined result (will be pushed to aggregator service)
+
 
